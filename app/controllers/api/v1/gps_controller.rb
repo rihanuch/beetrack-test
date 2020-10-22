7276fff0cc3c69a0dcaa7bf2b2b6e8b4264b791a
rihanuch <rihanuch@uc.cl>
2020-10-22 13:47:08 -0300

    [model] add: perform creation as jobs

--- app/controllers/api/v1/gps_controller.rb
+++ app/controllers/api/v1/gps_controller.rb
@@ -4,20 +4,11 @@ class Api::V1::GpsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    wp = Waypoint.create(parse_params)
+    GpsJob.perform_later(request_params)
     head(:ok)
   end
 
   private
-
-  def parse_params
-    {
-      lat: request_params[:latitude].to_f,
-      long: request_params[:longitude].to_f,
-      sent_at: DateTime.strptime(request_params[:sent_at], '%Y-%m-%d %H:%M:%S'),
-      vehicle: Vehicle.find_or_create_by(vehicle_identifier: request_params[:vehicle_identifier])
-    }
-  end
 
   def request_params
     # minimun parameters
